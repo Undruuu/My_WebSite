e16e6005504fedbadbcc55badb5b476b0ba85748
Andrey Bukin <andrey.gubaev@gmail.com>
2026-06-06 03:36:34 +0300

    Rename solo_projeckt -> solo_project

--- solo_projeckt/JS/burger__menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger__menu');
-    const nav = document.querySelector('.nav-menu');
-
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('active');
-        nav.classList.toggle('active');
-    });
-});
